86d1cce4c62d73acac8a46f789aec942a3bdaafe
abohoss <21p0141@eng.asu.edu.eg>
2024-08-19 23:56:04 +0300

    footer finished

--- goods.client/src/Components/Myfooter.jsx
+++ goods.client/src/Components/Myfooter.jsx
@@ -9,30 +9,9 @@ const Myfooter = () => {
     return (
         <div fixed="bottom">
             <MDBFooter id='footer' bgColor='light' className=' text-center text-lg-start text-muted'>
-                <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
-                    <div className='me-5 d-none d-lg-block'>
-                        <span>Get connected with us on social networks:</span>
-                    </div>
+                <section className='d-flex justify-content-center justify-content-lg-between p-2'>
 
-                    <div>
-                        <a href='' className='me-4 text-reset'>
-                            <MDBIcon color='secondary' fab icon='facebook-f' />
-                        </a>
-                        <a href='' className='me-4 text-reset'>
-                            <MDBIcon color='secondary' fab icon='twitter' />
-                        </a>
-                        <a href='' className='me-4 text-reset'>
-                            <MDBIcon color='secondary' fab icon='google' />
-                        </a>
-                        <a href='' className='me-4 text-reset'>
-                            <MDBIcon color='secondary' fab icon='instagram' />
-                        </a>
-                        <a href='' className='me-4 text-reset'>
-                            <MDBIcon color='secondary' fab icon='linkedin' />
-                        </a>
-                    </div>
                 </section>
-
                 <section className=''>
                     <MDBContainer className='text-center text-md-start mt-5'>
                         <MDBRow className='mt-3'>
@@ -122,9 +101,26 @@ const Myfooter = () => {
                     </MDBContainer>
                 </section>
 
-                <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
-                    © 2024 Copyright: BrandMatch.com
-                </div>
+                <MDBRow className=' p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+                    <MDBCol className='text-left'>© 2024 Copyright: BrandMatch.com</MDBCol>
+                    <MDBCol className='text-right'>
+                        <a href='' className='me-4 text-reset'>
+                            <MDBIcon color='secondary' fab icon='facebook-f' />
+                        </a>
+                        <a href='' className='me-4 text-reset'>
+                            <MDBIcon color='secondary' fab icon='twitter' />
+                        </a>
+                        <a href='' className='me-4 text-reset'>
+                            <MDBIcon color='secondary' fab icon='google' />
+                        </a>
+                        <a href='' className='me-4 text-reset'>
+                            <MDBIcon color='secondary' fab icon='instagram' />
+                        </a>
+                        <a href='' className='me-4 text-reset'>
+                            <MDBIcon color='secondary' fab icon='linkedin' />
+                        </a>
+                    </MDBCol>
+                </MDBRow>
             </MDBFooter>
         </div>
 
