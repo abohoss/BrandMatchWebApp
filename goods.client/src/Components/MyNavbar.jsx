6e8c1f6d7218304a74b355ad49e2f0c9c93b0a0a
Mark Saleh <mark-saleh@outlook.com>
2024-09-02 13:40:41 +0300

    Update navigation bar design

--- goods.client/src/Components/MyNavbar.jsx
+++ goods.client/src/Components/MyNavbar.jsx
@@ -1,27 +1,87 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import LogoNameRed from "../assets/images/LogoNameRed.svg";
 
 const MyNavbar = () => {
-    return (
-        <>
-            <Navbar sticky="top" style={{
-                backgroundColor: '#D12026'
-            }} data-bs-theme="dark">
-                <Container>
-                    <Navbar.Brand href="#home"><img src='src/assets/images/Logo Name.svg' width="150" height="50" className="d-inline-block align-top"
-                        alt="Logo"></img></Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
+  return (
+    <nav className="mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="flex h-16 items-center justify-between lg:h-20">
+        <div className="flex-shrink-0">
+          <a href="#" className="flex">
+            <img
+              className="h-8 w-auto"
+              src={LogoNameRed}
+              alt="Brandmatch logo"
+            />
+          </a>
+        </div>
 
-        </>
-    )
-}
+        <button
+          type="button"
+          className="inline-flex rounded-md p-2 text-black transition-all duration-200 hover:bg-gray-100 focus:bg-gray-100 lg:hidden"
+        >
+          <svg
+            className="block h-6 w-6"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 8h16M4 16h16"
+            ></path>
+          </svg>
 
-export default MyNavbar
+          <svg
+            className="hidden h-6 w-6"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            ></path>
+          </svg>
+        </button>
+
+        <div className="hidden lg:flex lg:items-center lg:justify-center lg:space-x-10">
+          <a
+            href="#about"
+            className="text-base text-black no-underline transition-all duration-200 hover:text-opacity-80"
+          >
+            About
+          </a>
+
+          <a
+            href="#services"
+            className="text-base text-black no-underline transition-all duration-200 hover:text-opacity-80"
+          >
+            Services
+          </a>
+
+          <a
+            href="#howTo"
+            className="text-base text-black no-underline transition-all duration-200 hover:text-opacity-80"
+          >
+            How to start
+          </a>
+        </div>
+
+        <a
+          href="#"
+          className="hidden items-center justify-center rounded-full border-2 border-bright-red bg-bright-red px-5 py-2.5 text-base font-semibold text-white/100 no-underline transition-all duration-200 hocus:bg-white hocus:text-bright-red lg:inline-flex"
+          role="button"
+        >
+          Start Now
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default MyNavbar;
