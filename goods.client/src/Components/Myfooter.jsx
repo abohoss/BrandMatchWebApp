f639df12133a0fe8584622c1a754a40b7f38ceb6
Mark Saleh <mark-saleh@outlook.com>
2024-09-18 16:13:31 +0300

    Improve footer design

--- goods.client/src/Components/Myfooter.jsx
+++ goods.client/src/Components/Myfooter.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import {
   MDBFooter,
   MDBContainer,
@@ -7,156 +7,199 @@ import {
   MDBIcon,
 } from "mdb-react-ui-kit";
 import "../styles/footer.css";
-const styles = {
-  fontFamily: "Montserrat",
-  color: "white",
-  backgroundColor: "#231F20",
-};
+
 const Myfooter = () => {
+  const [faceColor, setFaceColor] = useState("secondary");
+  const [twitterColor, setTwitterColor] = useState("secondary");
+  const [googleColor, setGoogleColor] = useState("secondary");
+  const [igColor, setIgColor] = useState("secondary");
+  const [liColor, setLiColor] = useState("secondary");
+
+  const enterSocial = (stateSetter) => {
+    stateSetter("primary");
+  };
+
+  const leaveSocial = (stateSetter) => {
+    stateSetter("secondary");
+  };
+
   return (
-    <div fixed="bottom" data-scroll-section>
+    <div data-scroll-section>
       <MDBFooter
         id="footer"
         bgColor="light"
         className="text-lg-start text-muted text-center"
       >
-        <section className="d-flex justify-content-center justify-content-lg-between p-2"></section>
-        <section className="">
-          <MDBContainer className="text-md-start mt-5 text-center">
-            <MDBRow className="mt-3">
-              <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4">
-                <MDBRow>
-                  <MDBCol md="3" lg="2" xl="2" className="mx-auto mb-4">
-                    <img
-                      src="assets/images/Logo.svg"
-                      width="50"
-                      height="20"
-                      className="d-inline-block align-top"
-                      alt="Logo"
-                    ></img>
-                  </MDBCol>
-                  <MDBCol md="9" lg="10" xl="10" className="mx-auto mb-4 mt-1">
-                    <h6 className="text-uppercase fw-bold mb-4">BrandMatch</h6>
-                  </MDBCol>
-                </MDBRow>
-                <p>
-                  Ready to maximize your brand's potential or revitalize your
-                  career? Contact us today to learn more about how BrandMatch
-                  can help you grow.
-                </p>
-              </MDBCol>
+        <MDBContainer className="text-md-start mt-3 p-2 text-center">
+          <MDBRow className="mt-3">
+            <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4">
+              <MDBRow>
+                <MDBCol md="3" lg="2" xl="2" className="mx-auto mb-4">
+                  <img
+                    src="assets/images/Logo.svg"
+                    width="50"
+                    height="20"
+                    className="d-inline-block align-top"
+                    alt="Logo"
+                  ></img>
+                </MDBCol>
+                <MDBCol md="9" lg="10" xl="10" className="mx-auto mb-4 mt-1">
+                  <h6 className="text-uppercase fw-bold mb-4">BrandMatch</h6>
+                </MDBCol>
+              </MDBRow>
+              <p className="text-sm">
+                Ready to maximize your brand&apos;s potential or revitalize your
+                career? Contact us today to learn more about how BrandMatch can
+                help you grow.
+              </p>
+            </MDBCol>
 
-              <MDBCol md="2" lg="2" xl="2" className="mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">Products</h6>
-                <p>
-                  <a href="#!" className="text-reset hover-link no-underline">
-                    Marketing
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset hover-link no-underline">
-                    Media Planning
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset hover-link no-underline">
-                    Campaign Ideas
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset hover-link no-underline">
-                    Logistics and Warehousing
-                  </a>
-                </p>
-              </MDBCol>
+            <MDBCol md="3" lg="2" xl="2" className="mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
+              <p>
+                <a
+                  href="#!"
+                  className="text-reset hover-link text-sm no-underline"
+                >
+                  About Us
+                </a>
+              </p>
+              <p>
+                <a
+                  href="#!"
+                  className="text-reset hover-link text-sm no-underline"
+                >
+                  Contact
+                </a>
+              </p>
+              <p>
+                <a
+                  href="#!"
+                  className="text-reset hover-link text-sm no-underline"
+                >
+                  Terms of Services
+                </a>
+              </p>
+              <p>
+                <a
+                  href="#!"
+                  className="text-reset hover-link text-sm no-underline"
+                >
+                  Privacy Policy
+                </a>
+              </p>
+            </MDBCol>
 
-              <MDBCol md="3" lg="2" xl="2" className="mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
-                <p>
-                  <a href="#!" className="text-reset hover-link no-underline">
-                    Pricing
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset hover-link no-underline">
-                    Settings
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset hover-link no-underline">
-                    Orders
-                  </a>
-                </p>
-                <p>
-                  <a href="#!" className="text-reset hover-link no-underline">
-                    Help
-                  </a>
-                </p>
-              </MDBCol>
-
-              <MDBCol md="4" lg="3" xl="3" className="mb-md-0 mx-auto mb-4">
-                <h6 className="text-uppercase fw-bold mb-4">Contact Us</h6>
+            <MDBCol md="6" lg="5" xl="5" className="mb-md-0 mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Contact Us</h6>
+              <p>
                 <a
                   href="https://www.google.com/maps/search/+41+Zaker+Hussein+St.+/@30.0437078,31.3347338,19.25z?entry=ttu&g_ep=EgoyMDI0MDkxMS4wIKXMDSoASAFQAw%3D%3DE"
                   target="_blank"
-                  className="text-reset hover-link no-underline"
+                  className="text-reset text-sm no-underline"
                 >
-                  <p>
-                    <MDBIcon color="secondary" icon="home" className="me-2" />
+                  <MDBIcon
+                    fas
+                    color="secondary"
+                    icon="map-marker-alt"
+                    className="me-3"
+                  />
+                  <span className="hover-link">
                     41 Zaker Hussien St. - Nasr City, Cairo - Egypt
-                  </p>
+                  </span>
                 </a>
+              </p>
 
+              <p>
                 <a
                   href="mailto:Info@brandmatch-eg.com"
-                  className="text-reset hover-link no-underline"
+                  className="text-reset text-sm no-underline"
                 >
-                  <p>
-                    <MDBIcon
-                      color="secondary"
-                      icon="envelope"
-                      className="me-3"
-                    />
-                    Info@brandmatch-eg.com
-                  </p>
+                  <MDBIcon color="secondary" icon="envelope" className="me-3" />
+                  <span className="hover-link">Info@brandmatch-eg.com</span>
                 </a>
+              </p>
 
+              <p>
                 <a
                   href="tel:+201018881991"
-                  className="text-reset hover-link no-underline"
+                  className="text-reset text-sm no-underline"
                 >
-                  <p>
-                    <MDBIcon color="secondary" icon="phone" className="me-3" />{" "}
-                    +2010 1888 1991
-                  </p>
+                  <MDBIcon color="secondary" icon="phone" className="me-3" />
+                  <span className="hover-link">+2010 1888 1991</span>
                 </a>
-              </MDBCol>
-            </MDBRow>
-          </MDBContainer>
-        </section>
+              </p>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
 
         <MDBRow
           className="p-4"
           style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
         >
-          <MDBCol className="text-left">
+          <MDBCol className="text-left text-xs">
             © 2024 Copyright: BrandMatch.org
           </MDBCol>
           <MDBCol className="text-right">
-            <a href="" className="text-reset me-4">
-              <MDBIcon color="secondary" fab icon="facebook-f" />
+            <a
+              href=""
+              className="text-reset me-4"
+              onMouseEnter={() => {
+                enterSocial(setFaceColor);
+              }}
+              onMouseLeave={() => {
+                leaveSocial(setFaceColor);
+              }}
+            >
+              <MDBIcon color={faceColor} fab icon="facebook-f" />
             </a>
-            <a href="" className="text-reset me-4">
-              <MDBIcon color="secondary" fab icon="twitter" />
+            <a
+              href=""
+              className="text-reset me-4"
+              onMouseEnter={() => {
+                enterSocial(setTwitterColor);
+              }}
+              onMouseLeave={() => {
+                leaveSocial(setTwitterColor);
+              }}
+            >
+              <MDBIcon color={twitterColor} fab icon="twitter" />
             </a>
-            <a href="" className="text-reset me-4">
-              <MDBIcon color="secondary" fab icon="google" />
+            <a
+              href=""
+              className="text-reset me-4"
+              onMouseEnter={() => {
+                enterSocial(setGoogleColor);
+              }}
+              onMouseLeave={() => {
+                leaveSocial(setGoogleColor);
+              }}
+            >
+              <MDBIcon color={googleColor} fab icon="google" />
             </a>
-            <a href="" className="text-reset me-4">
-              <MDBIcon color="secondary" fab icon="instagram" />
+            <a
+              href=""
+              className="text-reset me-4"
+              onMouseEnter={() => {
+                enterSocial(setIgColor);
+              }}
+              onMouseLeave={() => {
+                leaveSocial(setIgColor);
+              }}
+            >
+              <MDBIcon color={igColor} fab icon="instagram" />
             </a>
-            <a href="" className="text-reset me-4">
-              <MDBIcon color="secondary" fab icon="linkedin" />
+            <a
+              href=""
+              className="text-reset me-4"
+              onMouseEnter={() => {
+                enterSocial(setLiColor);
+              }}
+              onMouseLeave={() => {
+                leaveSocial(setLiColor);
+              }}
+            >
+              <MDBIcon color={liColor} fab icon="linkedin" />
             </a>
           </MDBCol>
         </MDBRow>
