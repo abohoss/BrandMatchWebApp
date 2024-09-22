57a79388da244c8096d8aff50f90d8342d206500
Mark Saleh <mark-saleh@outlook.com>
2024-09-22 23:11:23 +0300

    Make Images not draggable

--- goods.client/src/Components/Marquee/ImageMarquee.jsx
+++ goods.client/src/Components/Marquee/ImageMarquee.jsx
@@ -5,12 +5,13 @@ const ImageMarquee = ({ images, direction }) => {
   return (
     <Marquee pauseOnClick={true} speed={5} direction={direction}>
       {images.map((image, index) => (
-        <div
-          key={index}
-          className="m-1 rounded border border-black/50 p-4"
-          aria-hidden="true"
-        >
-          <img src={image} alt={`${image} logo`} className="max-h-16" />
+        <div key={index} className="m-1 rounded border border-black/50 p-4">
+          <img
+            src={image}
+            alt={`${image} logo`}
+            className="max-h-16"
+            draggable="false"
+          />
         </div>
       ))}
     </Marquee>
