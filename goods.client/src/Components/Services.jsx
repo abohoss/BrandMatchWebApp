bdeeb598d33bb31d92696a04cf81737bd41dbb41
Mark Saleh <mark-saleh@outlook.com>
2024-09-27 12:54:13 +0300

    Improve Services
    Add parallax movement inside Images
    Add more images to showcase Brandmatch projects
    Improve layout by adding margins around this section
    Improve readability by making background black

--- goods.client/src/Components/Services.jsx
+++ goods.client/src/Components/Services.jsx
@@ -3,7 +3,12 @@ import Market from "../assets/images/Services/supermarket.jpg";
 import Report from "../assets/images/Services/report.jpg";
 import Export from "../assets/images/Services/export.jpg";
 import Warehouse from "../assets/images/Services/warehouse.jpg";
-import { useEffect } from "react";
+import Delivery from "../assets/images/Services/delivery.jpeg";
+import Crush from "../assets/images/Services/crush.jpg";
+import Agdadna from "../assets/images/Services/agdadna.jpeg";
+import CrushStores from "../assets/images/Services/crush-stores.jpeg";
+import { useEffect, useState } from "react";
+import { useLocomotiveScroll } from "react-locomotive-scroll";
 
 const Services = ({ leftValues, setLeftValues }) => {
   const categroies = ["Sales", "Marketing", "Business Growth", "Logistics"];
@@ -99,26 +104,68 @@ const Services = ({ leftValues, setLeftValues }) => {
 
   const images = [
     {
-      src: Report,
-      top: "0rem",
-      left: "1vw",
+      src: CrushStores,
+      top: "10rem",
+      left: "5vw",
     },
     {
       src: Market,
-      top: "55rem",
-      left: "73vw",
+      top: "25rem",
+      left: "50vw",
+    },
+    {
+      src: Report,
+      top: "40rem",
+      left: "1vw",
+    },
+    {
+      src: Agdadna,
+      top: "65rem",
+      left: "48vw",
+    },
+    {
+      src: Crush,
+      top: "90rem",
+      left: "20vw",
     },
     {
       src: Export,
-      top: "105rem",
-      left: "8vw",
+      top: "170rem",
+      left: "5vw",
     },
     {
       src: Warehouse,
-      top: "125rem",
-      left: "25vw",
+      top: "190rem",
+      left: "48vw",
+    },
+    {
+      src: Delivery,
+      top: "205rem",
+      left: "1vw",
     },
   ];
+
+  const { scroll } = useLocomotiveScroll();
+  const [scrollPercentage, setScrollPercentage] = useState(0);
+  useEffect(() => {
+    if (scroll) {
+      scroll.on("scroll", (args) => {
+        const servicesBoundaries = document
+          .querySelector("#services")
+          .getBoundingClientRect();
+        // const topOffset = servicesBoundaries.top + args.scroll.y;
+        const bottomOffset = servicesBoundaries.bottom + args.scroll.y;
+
+        if (args.scroll.y <= bottomOffset) {
+          // const servicesHeight = bottomOffset - topOffset;
+          const servicesHeight = bottomOffset;
+          // const scrollDelta = args.scroll.y - topOffset;
+          const scrollDelta = args.scroll.y;
+          setScrollPercentage((scrollDelta / servicesHeight) * 100);
+        }
+      });
+    }
+  }, [scroll]);
 
   const randomSeed = (seed) => {
     let value = seed;
@@ -131,9 +178,9 @@ const Services = ({ leftValues, setLeftValues }) => {
   useEffect(() => {
     const updateLeftValues = () => {
       if (window.innerWidth < 768) {
-        setLeftValues(["0", "25", "33.3", "50"]);
+        setLeftValues(["0", "10", "20", "30", "40", "50"]);
       } else {
-        setLeftValues(["0", "25", "33.3", "50", "66.6", "75"]);
+        setLeftValues(["0", "12", "24", "36", "48", "60"]);
       }
     };
 
@@ -153,50 +200,60 @@ const Services = ({ leftValues, setLeftValues }) => {
   return (
     <section
       id="services"
-      className="relative px-1 md:px-4"
+      className="text-white"
+      style={{
+        backgroundImage:
+          "linear-gradient(to bottom, white 1%, #231f20 10%, #231f20 50%, #231f20 90%, white 99%)",
+      }}
       data-scroll-section
     >
-      {images.map((image, index) => (
-        <img
-          src={image.src}
-          alt="supermarket"
-          className="absolute w-1/4 max-w-lg -translate-x-1/2 -translate-y-1/2 rounded-lg"
-          style={{ top: image.top, left: image.left }}
-          key={index}
-          data-scroll
-          data-scroll-speed="2"
-        />
-      ))}
-
-      {services.map((service, index) => {
-        const leftValuesCopy = [...leftValues];
-        const noOfItemsToRemove =
-          lastPositionIdx == 0 || lastPositionIdx == leftValues.length - 1
-            ? 2
-            : 3;
-        lastPositionIdx = lastPositionIdx == 0 ? 1 : lastPositionIdx;
-        leftValuesCopy.splice(lastPositionIdx - 1, noOfItemsToRemove);
-        const generatedLeftValue =
-          leftValuesCopy[generator() % leftValuesCopy.length];
-        lastPositionIdx = leftValues.indexOf(generatedLeftValue);
-
-        return (
-          <div
+      <div className="relative mx-auto max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 lg:py-32">
+        {images.map((image, index) => (
+          <img
+            src={image.src}
+            alt="supermarket"
+            className="absolute max-w-lg -translate-x-1/2 -translate-y-1/2 rounded-lg object-cover"
+            style={{
+              width: "50vmin",
+              height: "25vmin",
+              top: image.top,
+              left: image.left,
+              objectPosition: `${scrollPercentage}% ${scrollPercentage}%`,
+            }}
             key={index}
-            className="h-40 w-full"
             data-scroll
-            data-scroll-speed="3"
-          >
-            <Service
-              no={service.category + 1}
-              name={service.name}
-              description={service.description}
-              category={categroies[service.category]}
-              leftValue={generatedLeftValue}
-            />
-          </div>
-        );
-      })}
+            data-scroll-speed="1"
+          />
+        ))}
+        {services.map((service, index) => {
+          const leftValuesCopy = [...leftValues];
+          const noOfItemsToRemove =
+            lastPositionIdx == 0 || lastPositionIdx == leftValues.length - 1
+              ? 2
+              : 3;
+          lastPositionIdx = lastPositionIdx == 0 ? 1 : lastPositionIdx;
+          leftValuesCopy.splice(lastPositionIdx - 1, noOfItemsToRemove);
+          const generatedLeftValue =
+            leftValuesCopy[generator() % leftValuesCopy.length];
+          lastPositionIdx = leftValues.indexOf(generatedLeftValue);
+          return (
+            <div
+              key={index}
+              className="h-60 w-full"
+              data-scroll
+              data-scroll-speed="1.5"
+            >
+              <Service
+                no={service.category + 1}
+                name={service.name}
+                description={service.description}
+                category={categroies[service.category]}
+                leftValue={generatedLeftValue}
+              />
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 };
