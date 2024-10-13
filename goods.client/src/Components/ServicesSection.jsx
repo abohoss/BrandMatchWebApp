b2e969102ce1e7d58daee3e283a3e07c9c8a8ed1
abohoss <21p0141@eng.asu.edu.eg>
2024-10-14 00:12:33 +0300

    all done

--- goods.client/src/Components/ServicesSection.jsx
+++ goods.client/src/Components/ServicesSection.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ServicesMobile from './ServicesMobile';
+import Accordion from 'react-bootstrap/Accordion';
 const ServiceCard = ({ bgColor, category, title, description, icon }) => (
   <div className={`w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-3`}>
     <div className={`h-full ${bgColor === 'dark' ? 'bg-[#231f20] text-white' : 'bg-white text-[#231f20]'} rounded-xl shadow flex flex-col justify-between`}>
       <div className="p-6 flex justify-between items-center">
         <div className="w-[50px] flex justify-center items-center">
-          <i className={icon}></i>
+          <img src={icon} />
         </div>
         <div className={`px-4 py-2 rounded-[32px] border ${bgColor === 'dark' ? 'border-white' : 'border-[#231f20]'}`}>
           <div className="text-xs ">{category}</div>
@@ -20,85 +20,106 @@ const ServiceCard = ({ bgColor, category, title, description, icon }) => (
 );
 
 const ServicesSection = () => {
-  const services = [
+  const customItemStyle = {
+    border: 'none',
+  };
+
+  const noFocusStyle = {
+    outline: 'none',
+    boxShadow: 'none',
+  };
+
+  const accordionHeaderStyle = {
+    ...noFocusStyle,
+    '&:focus': noFocusStyle,
+    '&:not(.collapsed)': noFocusStyle,
+  };
+  const sales = [
     {
       bgColor: 'dark',
       category: 'Sales',
       title: 'Sales Channels',
       description: 'We offer access to over 2,000 sales channels to maximize your product reach.',
-      icon: "fa-solid fa-universal-access"
-    },
-    {
-      bgColor: 'light',
-      category: 'Marketing',
-      title: 'Online and Offline Marketing Activities',
-      description: 'We handle all aspects of marketing, both digital and traditional, to boost your brand\'s visibility.',
-      icon: 'fa-solid fa-lightbulb'
-    },
-    {
-      bgColor: 'dark',
-      category: 'Logistics',
-      title: 'Warehousing',
-      description: 'Provide warehousing per pallet to meet your storage needs.',
-      icon: 'fa fa-warehouse'
+      icon: "/assets/images/channels.svg"
     },
     {
       bgColor: 'light',
       category: 'Sales',
       title: 'Broad Store Access',
       description: 'Access to all hyper markets, large grocery stores, retail markets, small groceries and different channels in Egypt',
-      icon: 'fa-solid fa-store'
-    },
-    {
-      bgColor: 'dark',
-      category: 'Marketing',
-      title: 'Market Research',
-      description: 'We conduct thorough research on competitors, prices, channels, and more to inform your strategy.',
-      icon: 'fa-solid fa-book-open'
-    },
-    {
-      bgColor: 'light',
-      category: 'Logistics',
-      title: 'Product Delivery',
-      description: 'Deliver your products to the desired location efficiently.',
-      icon: 'fa-solid fa-truck-arrow-right'
+      icon: '/assets/images/house.svg'
     },
     {
       bgColor: 'dark',
       category: 'Sales',
       title: 'Monthly Sales Reports',
       description: 'Stay informed with detailed sales reports and market overviews.',
-      icon: 'fa-solid fa-flag'
-    },
-    {
-      bgColor: 'light',
-      category: 'Marketing',
-      title: 'Shelf Organization',
-      description: 'Ensure your products have the best shelf view in hypermarkets.',
-      icon: 'fa-solid fa-sitemap'
-    },
-    {
-      bgColor: 'dark',
-      category: 'Logistics',
-      title: 'Order Processing',
-      description: 'Handle labeling and invoicing to streamline your order process.',
-      icon: 'fa-solid fa-microchip'
+      icon: '/assets/images/chart.svg'
     },
     {
       bgColor: 'light',
       category: 'Sales',
       title: 'Product Availability',
       description: 'Ensure your products are widely available at competitive prices across all hypermarkets.',
-      icon: 'fa-brands fa-product-hunt'
+      icon: '/assets/images/av.svg'
+    }
+  ];
+
+  const marketing = [
+    {
+      bgColor: 'light',
+      category: 'Marketing',
+      title: 'Online and Offline Marketing Activities',
+      description: 'We handle all aspects of marketing, both digital and traditional, to boost your brand\'s visibility.',
+      icon: '/assets/images/marketingActivities.svg'
     },
+    {
+      bgColor: 'dark',
+      category: 'Marketing',
+      title: 'Market Research',
+      description: 'We conduct thorough research on competitors, prices, channels, and more to inform your strategy.',
+      icon: '/assets/images/market.svg'
+    },
+    {
+      bgColor: 'light',
+      category: 'Marketing',
+      title: 'Shelf Organization',
+      description: 'Ensure your products have the best shelf view in hypermarkets.',
+      icon: '/assets/images/shelf.svg'
+    },
+
     {
       bgColor: 'dark',
       category: 'Marketing',
       title: 'Co-Promotion',
       description: 'Partner with complementary products to enhance your market presence.',
-      icon: 'fa-solid fa-rectangle-ad'
+      icon: '/assets/images/promotion.svg'
     }
-  ];
+  ]
+
+  const logisitcs = [
+    {
+      bgColor: 'dark',
+      category: 'Logistics',
+      title: 'Warehousing',
+      description: 'Provide warehousing per pallet to meet your storage needs.',
+      icon: '/assets/images/warehouse.svg'
+    },
+    {
+      bgColor: 'light',
+      category: 'Logistics',
+      title: 'Product Delivery',
+      description: 'Deliver your products to the desired location efficiently.',
+      icon: '/assets/images/delivery.svg'
+    },
+    {
+      bgColor: 'dark',
+      category: 'Logistics',
+      title: 'Order Processing',
+      description: 'Handle labeling and invoicing to streamline your order process.',
+      icon: '/assets/images/process.svg'
+    }
+  ]
 
   return (
     <div data-scroll-section>
@@ -107,12 +128,43 @@ const ServicesSection = () => {
           <div className="py-12 md:py-24 container mx-auto px-4">
             <h2 className="text-4xl md:text-7xl text-center mb-8 md:mb-20">Services</h2>
             <div className="flex flex-wrap -mx-3 hidden sm:flex ">
-              {services.map((service, index) => (
+              {sales.map((service, index) => (
+                <ServiceCard key={index} {...service} />
+              ))}
+              {marketing.map((service, index) => (
+                <ServiceCard key={index} {...service} />
+              ))}
+              {logisitcs.map((service, index) => (
                 <ServiceCard key={index} {...service} />
               ))}
             </div>
             <div className="block sm:hidden">
-              <ServicesMobile />
+              <Accordion>
+                <Accordion.Item eventKey="0" style={customItemStyle}>
+                  <Accordion.Header style={accordionHeaderStyle}>Sales</Accordion.Header>
+                  <Accordion.Body>
+                    {sales.map((service, index) => (
+                      <ServiceCard key={index} {...service} />
+                    ))}
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1" style={customItemStyle}>
+                  <Accordion.Header style={accordionHeaderStyle}>Marketing</Accordion.Header>
+                  <Accordion.Body>
+                    {marketing.map((service, index) => (
+                      <ServiceCard key={index} {...service} />
+                    ))}
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2" style={customItemStyle}>
+                  <Accordion.Header style={accordionHeaderStyle}>Logistics</Accordion.Header>
+                  <Accordion.Body>
+                    {logisitcs.map((service, index) => (
+                      <ServiceCard key={index} {...service} />
+                    ))}
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
             </div>
           </div>
         </div>
@@ -122,3 +174,4 @@ const ServicesSection = () => {
 };
 
 export default ServicesSection;
+
